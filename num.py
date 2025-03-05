9726f7ddbf4a7872b5fc785720a30e8f6877871b
Cristina <ab.cristina00@gmail.com>
2025-03-05 19:13:42 +0800

    Numeric Types

---
+++ num.py
@@ -0,0 +1,39 @@
+add = 2 + 3
+print(add)
+
+subtract = 2 - 3
+print(subtract)
+
+exponents = 3 ** 3 #3^3
+print(exponents)
+
+operations_order = (2 + 3) * 4
+print (operations_order)
+
+floating = 3.14 + 4.02
+print (floating)
+
+mixIntFloat = 3.14 + 2 #output will still be float
+print(mixIntFloat)
+
+#when writing long numbers, you can use underscores for readability and concise
+longnum = 14_000_000_000
+print(longnum)
+#the output would still be 14000000000
+
+#CONSTANT/PERMANENTVALUE 
+#to considered a value that is permanent/constant, it should have an ALL CAPITAL LETTER
+PI = 3.14159265359
+print(PI)
+
+print ("=======")
+#Write addition, subtraction, multiplication, and division op that each result in the number 8
+
+print(5+3)
+print(10-2)
+print(2*4)
+print(16/2)
+
+
+fav_num = 22
+print(f"My favorite number is: {fav_num}")
