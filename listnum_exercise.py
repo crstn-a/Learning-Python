fe2af349333f3d6119fb7088757eadd49c900a9a
Cristina <ab.cristina00@gmail.com>
2025-03-06 21:27:01 +0800

    Numerical List - Exercise

---
+++ listnum_exercise.py
@@ -0,0 +1,18 @@
+#Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive
+
+for numOnetoTwenty in range (1, 21):
+    print(numOnetoTwenty)
+
+#list from one to one million
+#for oneMillion in list(range(1,1000000)):
+    #print(oneMillion)
+
+#print(min(oneMillion))
+#print(max(oneMillion))
+#print(sum(oneMillion))
+
+#ctrl c - if the output is taking too long
+
+#odd num through 1 - 20
+for oddNum in range(1, 21, 2):
+    print(oddNum)
